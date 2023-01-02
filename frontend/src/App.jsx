66181b8f1dc3170481db1b1732f0728c5e7202da
rhodinemma <rhodinemma10@gmail.com>
2023-01-02 13:43:46 +0300

    ft(add-card-layouts)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -179,7 +179,39 @@ function App() {
       <div className="container">
         <h1>Data Visualization Project</h1>
         <br />
-        <br />
+
+        <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
+          <div className="col">
+            <div className="card h-100 bg-info">
+              <div className="card-body">
+                <h5 className="card-title">Sectors</h5>
+                <p className="card-text" style={{ fontSize: "4rem" }}>
+                  {sector.length}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card h-100 bg-warning">
+              <div className="card-body">
+                <h5 className="card-title">Regions</h5>
+                <p className="card-text" style={{ fontSize: "4rem" }}>
+                  {region.length}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card h-100 bg-success text-white">
+              <div className="card-body">
+                <h5 className="card-title">Countries</h5>
+                <p className="card-text" style={{ fontSize: "4rem" }}>
+                  {country.length}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="row mb-5">
           <div className="col-lg-12">
